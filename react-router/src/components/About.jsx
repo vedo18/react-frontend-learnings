5442b9c0eb7e23ca818b466cb95885560d5dd87f
vedo18 <saketvats18@gmail.com>
2024-07-21 16:24:32 +0530

    Setup of react-router

---
+++ react-router/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <h2>This is the About page of the react -router application</h2>
+        </div>
+    );
+};
+
+export default About;
